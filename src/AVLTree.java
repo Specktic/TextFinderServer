58828dd486af6b353526e3b9bb333801128c6e04
Specktic <111095859+Specktic@users.noreply.github.com>
2022-10-17 02:05:20 -0600

    - adapt AVLTree class because of AVLNode encapsulation

--- src/AVLTree.java
+++ src/AVLTree.java
@@ -4,7 +4,7 @@ public class AVLTree {
 
     /** method for getting height of a node */
     private int height (AVLNode t){
-        return t == null ? -1 : t.height;
+        return t == null ? -1 : t.getHeight();
     }
 
     /** method for balancing the tree*/
@@ -12,53 +12,53 @@ public class AVLTree {
         if (t == null){
             return null;
         }
-        if (height(t.left) - height(t.right) > TOLERANCE){
-            if (height(t.left.left) >= height(t.left.right))
+        if (height(t.getLeft()) - height(t.getRight()) > TOLERANCE){
+            if (height(t.getLeft().getLeft()) >= height(t.getLeft().getRight()))
                 t = rotateLeft(t);
             else
                 t = doubleLeft(t);
         } else {
-            if (height(t.right) - height(t.left) > TOLERANCE){
-                if (height(t.right.right) >= height(t.right.left))
+            if (height(t.getRight()) - height(t.getLeft()) > TOLERANCE){
+                if (height(t.getRight().getRight()) >= height(t.getRight().getLeft()))
                     t = rotateRight(t);
                 else
                     t = doubleRight(t);
             }
         }
-        t.height = Math.max(height(t.left), height(t.right)) + 1;
+        t.setHeight(Math.max(height(t.getLeft()), height(t.getRight())) + 1);
         return t;
     }
 
     /** method for performing a single left rotation */
     private AVLNode rotateLeft(AVLNode m){
-        AVLNode m1 = m.left;
-        m.left = m1.right;
-        m1.right = m;
-        m.height = Math.max(height(m.left), height(m.right)) + 1;
-        m1.height = Math.max(height(m1.left), m.height) + 1;
+        AVLNode m1 = m.getLeft();
+        m.setLeft(m1.getRight());
+        m1.setRight(m);
+        m.setHeight(Math.max(height(m.getLeft()), height(m.getRight())) + 1);
+        m1.setHeight(Math.max(height(m1.getLeft()), m.getHeight()) + 1);
         return m1;
     }
 
     /** method for performing a double left rotation */
     private AVLNode doubleLeft(AVLNode m){
-        m.left = rotateRight(m.left);
+        m.setLeft(rotateRight(m.getLeft()));
         return rotateLeft(m);
     }
 
     /** method for performing a single right rotation */
     private AVLNode rotateRight(AVLNode m){
-        AVLNode m1 = m.right;
-        m.right = m1.left;
-        m1.left = m;
-        m.height = Math.max(height(m.right), height(m.left)) + 1;
-        m1.height = Math.max(height(m1.right), m.height) + 1;
+        AVLNode m1 = m.getRight();
+        m.setRight(m1.getLeft());
+        m1.setLeft(m);
+        m.setHeight(Math.max(height(m.getRight()), height(m.getLeft())) + 1);
+        m1.setHeight(Math.max(height(m1.getRight()), m.getHeight()) + 1);
         return m1;
     }
 
     /** method for performing a double right rotation */
     private AVLNode doubleRight(AVLNode m){
-        m.right = rotateLeft(m.right);
-        return rotateRight(m);
+        m.setRight(rotateLeft(m.getRight()));
+        return rotateLeft(m);
     }
 }
 
