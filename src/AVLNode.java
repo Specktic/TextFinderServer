92ffa8ad0aa9c501cd349defa1267a13222cade5
Specktic <111095859+Specktic@users.noreply.github.com>
2022-10-17 01:06:04 -0600

    - give basic functionality to AVL node

--- src/AVLNode.java
+++ src/AVLNode.java
@@ -1,2 +1,21 @@
+import java.util.List;
+
 public class AVLNode{
+    String word;
+    List occurrence;
+    AVLNode left;
+    AVLNode right;
+    int height;
+
+    public AVLNode(String word, List occurrence){
+        this(word, occurrence, null, null);
+    }
+
+    public AVLNode(String word, List occurrence, AVLNode left, AVLNode right){
+        this.word = word;
+        this.occurrence = occurrence;
+        this.left = left;
+        this.right = right;
+        this.height = 0;
+    }
 }
