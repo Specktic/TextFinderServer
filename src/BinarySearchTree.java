a471f4c572e06eafd2d0aa8413cbc6187c34fcd2
Specktic <111095859+Specktic@users.noreply.github.com>
2022-10-17 02:49:37 -0600

    - implement BinarySearchTree with crude empty(), contain() and insert() methods

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class BinarySearchTree {
 
     /** constructor */
@@ -7,4 +10,53 @@ public class BinarySearchTree {
 
     /** attribute 1, a binary node */
     private BinaryNode root;
+
+    public boolean empty(){
+        return this.root == null;
+    }
+
+    public boolean contain(String word, BinaryNode node){
+        if (node == null){
+            return false;
+        } else {
+            int compare = word.compareTo(node.getWord());
+
+            if (compare < 0){
+                return contain(word, node.getLeft());
+            } else if (compare > 0) {
+                return contain(word, node.getRight());
+            } else {
+                return true;
+            }
+        }
+    }
+
+    public void insert(String word) {
+        List Occurrence = new LinkedList();
+        Occurrence.add(1);
+        this.root = this.insert(word, Occurrence, this.root);
+    }
+    public BinaryNode insert(String word, List occurrence, BinaryNode node){
+        if ( node == null){
+            List Occurrence = new LinkedList();
+            Occurrence.add(1);
+            return new BinaryNode(word, Occurrence ,null, null);
+        }
+
+        int compare = word.compareTo(node.getWord());
+
+        if (compare < 0){
+            List Occurrence = new LinkedList();
+            Occurrence.add(1);
+            node.setLeft(this.insert(word, Occurrence, node.getLeft()));
+        } else if (compare > 0){
+            List Occurrence = new LinkedList();
+            Occurrence.add(1);
+            node.setRight(this.insert(word, Occurrence, node.getRight()));
+        } else {
+            node.getOccurrence().add(1);
+        }
+
+        return node;
+    }
 }
