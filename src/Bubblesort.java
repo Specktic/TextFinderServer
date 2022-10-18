08658ef2ab80e7274c9f370019b09d1e941911c2
Specktic <111095859+Specktic@users.noreply.github.com>
2022-10-17 19:29:59 -0600

    - implement Bubblesort algorithm for sorting results by name

--- src/Bubblesort.java
+++ src/Bubblesort.java
@@ -1,4 +1,22 @@
+import java.util.*;
+
 public class Bubblesort {
+    /** constructor */
+    public Bubblesort(){}
 
+    /** sorting by bubblesort method */
+    public static void sort(String[] a) {
+        int in;
+        int out;
 
+        for (out = a.length - 1; out > 1; out--) {
+            for (in = 0; in < out; in++) {
+                if (a[in].compareTo(a[in + 1]) > 0) {
+                    String temp = a[in];
+                    a[in] = a[in + 1];
+                    a[in + 1] = temp;
+                }
+            }
+        }
+    }
 }
